6492411eb7979cb41ae95eb6ee0a13c2e8691654
Obeida Iqbal <oiqbal@umich.edu>
2025-01-25 17:23:22 -0500

    created an aws ec2 instance and updated the backend to work with flask, then used the ec2 instance to host the flask enviornment to process movie search queries

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Movie Streaming Extension
-This is meant to be an extension that allows you to search for the most accurate streaming and renting data for a movie, this is intended to work hand and hand with letterboxd. The program uses a api from [https://rapidapi.com/hub](https://rapidapi.com/hub) that pulls the most current streaming and renting data
+This is meant to be an extension that allows you to search for the most accurate streaming and renting data for a movie, this is intended to work hand and hand with letterboxd. The program uses a api from [https://rapidapi.com/hub](https://rapidapi.com/hub) to pull the most current streaming and renting data. It then uses an AWS EC2 instance to host a Flask backend that processes movie search queries. The backend fetches data from the API, cleans and formats it, and returns structured JSON responses. The Chrome extension (will eventually) interacts with this backend by sending user-inputted movie titles, retrieving availability details, and displaying streaming and rental options for the user in real time.
 
 Pushing: 
   ```sh
@@ -10,4 +10,15 @@ Pushing:
 Pulling:
   ```sh
   git pull origin main
+  ```
+To Run the main.py flask instance locally:
+1. Activate the virtual enviornment & run main.py
+  ```sh
+  source venv/bin/Activate
+  cd backend
+  python3 main.py
+  ```
+2. In a web broswer enter to test a seach (update address portion with whats in terminal)
+  ```sh
+  {address}/get-movie?title=Star%20Wars
   ```
--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import http.client
 import urllib.parse
 import json
 
+app = Flask(__name__)
+CORS(app) 
 
-def print_data_output(data):
-    parsedData = clean_data(data)
-
-    with open("output.json", "w", encoding="utf-8") as json_file:
-        json.dump(parsedData, json_file, indent=4)
+def read_key():
+    file = open("apikey.txt", "r")
+    return file.read()
 
 def clean_data(data):
-    parsedData = json.loads(data)
-    
+    try:
+        parsedData = json.loads(data)
+    except json.JSONDecodeError:
+        return {"error": "Failed to decode API response"}
+
     cleaned_output = []
 
     for item in parsedData:
@@ -22,36 +27,28 @@ def clean_data(data):
             "Genres": [genre["name"] for genre in item.get("genres", [])],
             "Directors": item.get("directors", []),
             "Cast": item.get("cast", []),
-            "Overview": item.get("overview", []),
+            "Overview": item.get("overview", ""),
             "Streaming Options": [],
             "Image": item.get("imageSet", {}).get("verticalPoster", {}).get("w480", "")
         }
+
         if "streamingOptions" in item and "us" in item["streamingOptions"]:
             for option in item["streamingOptions"]["us"]:
+                price = option.get("price", {}).get("formatted", "subscription")
+                if option["type"] == "addon":
+                    price = option["service"].get("addon", {}).get("name", "addon")
                 movie_info["Streaming Options"].append({
                     "Service": option["service"]["name"],
                     "Type": option["type"].capitalize(),
-                    "Price": (
-                        option["service"].get("addon", {}).get("name", "addon") if option["type"] == "addon"
-                        else option.get("price", {}).get("formatted", "subscription")
-                    ),
-                    
+                    "Price": price,
                     "Link": option.get("link", "N/A")
                 })
-                pass
+
         cleaned_output.append(movie_info)
 
     return cleaned_output
-    
-def get_encoded_title():
-    movie = input("Enter Movie Title: ")
-    return urllib.parse.quote(movie)
 
-def read_key():
-    file = open("backend/apikey.txt", "r")
-    return file.read()
-
-def get_movie_data():
+def get_movie_data(title):
     conn = http.client.HTTPSConnection("streaming-availability.p.rapidapi.com")
 
     headers = {
@@ -59,16 +56,22 @@ def get_movie_data():
         'x-rapidapi-host': "streaming-availability.p.rapidapi.com"
     }
 
-    conn.request("GET", f"/shows/search/title?country=us&title={get_encoded_title()}&show_type=movie&output_language=en", headers=headers)
+    conn.request("GET", f"/shows/search/title?country=us&title={urllib.parse.quote(title)}&show_type=movie&output_language=en", headers=headers)
 
     res = conn.getresponse()
     data = res.read()
+    return data.decode("utf-8")
 
-    # print(data.decode("utf-8"))
-    print_data_output(data)
+@app.route('/get-movie', methods=['GET'])
+def get_movie():
+    title = request.args.get('title')
+    if not title:
+        return jsonify({"error": "Missing 'title' parameter"}), 400
 
-def main():
-    get_movie_data()
+    raw_data = get_movie_data(title)
+    cleaned_output = clean_data(raw_data)
+
+    return jsonify(cleaned_output)
 
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0', port=5000)  # Run Flask on port 5000
