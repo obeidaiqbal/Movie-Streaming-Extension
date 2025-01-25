6acd9e8efe05fbb515d973a025d3834a72ca7fdb
Obeida Iqbal <oiqbal@umich.edu>
2025-01-25 13:43:31 -0500

    updated extension to be able to take in a user input

--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,12 @@
-console.log("This is a popup!")
+console.log("This is a test popup!")
+
+document.addEventListener("DOMContentLoaded", function () {
+    const inputField = document.getElementById("mtitle");
+    
+    inputField.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") { 
+            event.preventDefault(); 
+            console.log("User Input:", inputField.value.trim());
+        }
+    });
+});
