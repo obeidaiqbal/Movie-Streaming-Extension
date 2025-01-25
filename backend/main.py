6319a6fa96277757b5f041850c68aea93f927c1a
Obeida Iqbal <oiqbal@umich.edu>
2025-01-25 16:04:27 -0500

    cleaned up the retreived data to only show relevant information

--- backend/main.py
+++ backend/main.py
@@ -4,11 +4,45 @@ import json
 
 
 def print_data_output(data):
-    parsedData = json.loads(data)
+    parsedData = clean_data(data)
 
     with open("output.json", "w", encoding="utf-8") as json_file:
         json.dump(parsedData, json_file, indent=4)
 
+def clean_data(data):
+    parsedData = json.loads(data)
+    
+    cleaned_output = []
+
+    for item in parsedData:
+        movie_info = {
+            "Title": item.get("title"),
+            "Year": item.get("releaseYear"),
+            "Runtime": f"{item.get('runtime', 0)} minutes",
+            "Genres": [genre["name"] for genre in item.get("genres", [])],
+            "Directors": item.get("directors", []),
+            "Cast": item.get("cast", []),
+            "Overview": item.get("overview", []),
+            "Streaming Options": [],
+            "Image": item.get("imageSet", {}).get("verticalPoster", {}).get("w480", "")
+        }
+        if "streamingOptions" in item and "us" in item["streamingOptions"]:
+            for option in item["streamingOptions"]["us"]:
+                movie_info["Streaming Options"].append({
+                    "Service": option["service"]["name"],
+                    "Type": option["type"].capitalize(),
+                    "Price": (
+                        option["service"].get("addon", {}).get("name", "addon") if option["type"] == "addon"
+                        else option.get("price", {}).get("formatted", "subscription")
+                    ),
+                    
+                    "Link": option.get("link", "N/A")
+                })
+                pass
+        cleaned_output.append(movie_info)
+
+    return cleaned_output
+    
 def get_encoded_title():
     movie = input("Enter Movie Title: ")
     return urllib.parse.quote(movie)
